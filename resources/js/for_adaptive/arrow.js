b50ea5167f66a39ecbed7d09006971438b8d1331
Alexander129 <000777000casa@gmail.com>
2025-09-25 00:42:26 +0300

    the appearance of arrows when changing the screen

--- resources/js/for_adaptive/arrow.js
+++ resources/js/for_adaptive/arrow.js
@@ -4,19 +4,71 @@
 //
 
 // global variables
-// for elements header
+// screen size
+const screenWidth = window.screen.width;
+
+// btn arrows
 const btn_show_menu = document.getElementById("btn_show_menu");
 const btn_close_menu = document.getElementById("btn_close_menu");
+const arrows = document.getElementById("arrows");
+
+// header
+const header_logo = document.getElementById("header_logo");
+const header_menu_list = document.getElementById("header_menu");
+const header_btn_entry = document.getElementById("header_actions");
+const header = document.getElementById('up');
+
+
+if (screenWidth > 768) {
+        // hide btn for adaptive
+        btn_show_menu.style.display = 'none';
+        btn_close_menu.style.display = 'none';
+        arrows.style.display = 'none';
+        // show header
+        header_logo.style.display = 'flex';
+        header_menu.style.display = 'flex';
+        header_btn_entry.style.display = 'initial';
+
+        header.classList.toggle("show");
+    } else if (screenWidth < 768 ){
+        // visible btn
+        arrows.style.display = 'inital';
+        btn_show_menu.style.display = 'initial';
+        btn_close_menu.style.display = 'none';  
+
+        // hide header
+        header_logo.style.display = 'none';
+        header_menu.style.display = 'none';
+        header_btn_entry.style.display = 'none';
+
+        header.classList.remove("show");
+        header.classList.toggle("close");
+    }
 
 // first we check the screen size
 window.addEventListener('resize', (e) => {
-    const screenWidth = document.body.clientWidth;
-    const header = document.getElementById("header_menu");
+    const screen_width = document.body.clientWidth;
 
-    if (screenWidth > 768) {
-        btn
+    if (screen_width > 768) {
+        // hide btn for adaptive
+        arrows.style.display = 'none';
+        btn_show_menu.style.display = 'none';
+        btn_close_menu.style.display = 'none';
+        // show header
+        header_logo.style.display = 'flex';
+        header_menu.style.display = 'flex';
+        header_btn_entry.style.display = 'initial';
+
         header.classList.toggle("show");
-    } else if (screenWidth < 768 ){
+    } else if (screen_width < 768 ){
+        // visible btn
+        arrows.style.display = 'flex';
+        btn_show_menu.style.display = 'initial'; 
+        btn_close_menu.style.display = 'none';  
+        // hide header
+        header_logo.style.display = 'none';
+        header_menu.style.display = 'none';
+        header_btn_entry.style.display = 'none';
 
         header.classList.remove("show");
         header.classList.toggle("close");
@@ -24,4 +76,5 @@ window.addEventListener('resize', (e) => {
 
     
 });
+
 
