6e7015d58851773171f3dad2dc6325797264d577
Alexander129 <000777000casa@gmail.com>
2025-09-27 18:16:03 +0300

    add documentation for programmers

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('index');
 });
+
+// for staticsic the pages
+Route::get('/page/{slug}', function ($slug) {
+    if (view()->exists("pages.{$slug}")) {
+        return view("pages.{$slug}");
+    }
+    abort(404);
+});
