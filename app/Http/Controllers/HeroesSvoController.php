911ad73dc2f21537f9e6cf9defde86e61d89a02e
Alexander129 <000777000casa@gmail.com>
2025-10-15 23:27:48 +0300

    added files

---
+++ app/Http/Controllers/HeroesSvoController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use App\Models\city_heroes;
+
+class HeroesSvoController extends Controller
+{
+    public function show(Request $request)
+    {
+        $user = Auth::user();
+
+        if ($user->isBan) {
+            return redirect()->route('profile_banned');
+        }
+
+        $heroesSvo = city_heroes::with('user')->get()->where('type', 'СВО');
+
+        return view('profile.heroes_svo', compact('heroesSvo'));
+    }
+}
